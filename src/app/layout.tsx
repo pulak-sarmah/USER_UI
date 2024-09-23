1dd5c0156cb86b3da4e6bba083c50dce0a7ce67d
pulak-sarmah <pulaksarmah333@gmail.com>
2024-09-23 18:09:14 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import React from "react";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Lazziz Pizzeria",
+  description:
+    " Lazziz Pizzeria is a restaurant in the heart of the city. We offer a wide range of pizzas, pastas, and salads. We also have a variety of desserts and drinks. Come and enjoy our delicious food and friendly service!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
